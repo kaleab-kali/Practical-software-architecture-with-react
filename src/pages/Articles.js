6a314e83e0bf8cb4a0d2b5a9cdadfa77b0f1f7c6
kaliktg <86680782+kaliktg@users.noreply.github.com>
2023-04-01 15:25:03 +0000

    added routes for the pages

--- src/pages/Articles.js
+++ src/pages/Articles.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 export const Articles = () => {
   return (
     <div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,9 @@
-export const home = () => {
-    return (
-        <div>
-        <h1>Home</h1>
-        </div>
-    );
-}
+import React from "react";
+
+export const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+};
